81cc524fc48aac455a5874e7777ac2bcfcf91363
Obi-Wan <cayton10@live.marshall.edu>
2021-02-17 16:18:19 -0500

    Working on Loading Next Activity

---
+++ A2/ActivitiesandIntents/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+//Namespace for creating explicit intent
+using Android.Content;
+
+namespace ActivitiesandIntents
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+
+            FindViewById<Button>(Resource.Id.callSegue).Click += MainCallButton_Push;
+
+        }
+
+        //Method to "segue" between activities / views for calling
+        private void MainCallButton_Push(object sender, EventArgs e)
+        {
+            var intent = new Intent(this, typeof(CallActivity));
+
+            StartActivity(intent);
+        }
+
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
