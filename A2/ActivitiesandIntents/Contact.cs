3362e619d9b616e495bba59e80f4f57ea2a73a01
Obi-Wan <cayton10@live.marshall.edu>
2021-02-18 12:43:07 -0500

    Map function works

---
+++ A2/ActivitiesandIntents/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ActivitiesandIntents
+{
+    public class Contact
+    {
+        private String location;
+        private String email;
+        //Just construct w/ default for Dr. Mauro
+        public Contact()
+        {
+            this.location = "Morrow+Library+Huntington+WV";
+            this.email = "maurod@marshall.edu";
+        }
+
+        public String Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+    }
+}
--- A2/ActivitiesandIntents/MainActivity.cs
+++ A2/ActivitiesandIntents/MainActivity.cs
@@ -14,6 +14,9 @@ namespace ActivitiesandIntents
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        //Construct Dr. Mauro for this application
+        Contact drMauro = new Contact();
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -21,7 +24,31 @@ namespace ActivitiesandIntents
             SetContentView(Resource.Layout.Main);
 
             FindViewById<Button>(Resource.Id.callSegue).Click += MainCallButton_Push;
+            FindViewById<Button>(Resource.Id.openEmail).Click += OpenEmail_Push;
+            FindViewById<Button>(Resource.Id.openMap).Click += OpenMap_Push;
 
+        }
+
+        private void OpenMap_Push(object sender, EventArgs e)
+        {
+            //Get the location from our dr mauro object
+            String location = drMauro.Location.ToString();
+
+            var intent = new Intent();
+            intent.SetAction(Intent.ActionView);
+
+            intent.SetData(Android.Net.Uri.Parse($"geo:0,0?q={location}"));
+
+            //Lick stamp and send IF we have an application that can accomplish
+            if (intent.ResolveActivity(PackageManager) != null)
+            {
+                StartActivity(intent);
+            }
+        }
+        
+        private void OpenEmail_Push(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         //Method to "segue" between activities / views for calling
