e0944bc600eb4889d1786d85e587d28715a1909e
Obi-Wan <cayton10@live.marshall.edu>
2021-03-12 22:29:44 -0500

    Versioning UI Layouts

---
+++ A3/DataPersistence/Contact.cs
@@ -0,0 +1,65 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace DataPersistence
+{
+    public class Contact
+    {
+        private string first;
+        private string last;
+        private string email;
+        private string phone;
+
+        /**
+         * Public constructor takes four string parameters:
+         * first name, last name, email, and phone number
+         */
+        public Contact(string f, string l, string e, string p)
+        {
+            this.first = f;
+            this.last = l;
+            this.email = e;
+            this.phone = p;
+        }
+
+        //Getters / setters
+
+        public string FirstName
+        {
+            get { return first; }
+            set { first = value; }
+        }
+
+        public string LastName
+        {
+            get { return last; }
+            set { last = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        public override string ToString()
+        {
+            return ($"{first} {last}");
+        }
+    }
+}
---
+++ A3/DataPersistence/MainActivity.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Android.Content;
+using System.Collections.Generic;
+
+namespace DataPersistence
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        public static List<Contact> Contacts = new List<Contact>();
+
+        
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+
+            Contacts.Add(new Contact("Benjamin", "Cayton", "cayton10@live.marshall.edu", "3046389603"));
+            Contacts.Add(new Contact("Benjamin", "Cayton", "cayton10@live.marshall.edu", "3046389603"));
+            Contacts.Add(new Contact("Benjamin", "Cayton", "cayton10@live.marshall.edu", "3046389603"));
+            Contacts.Add(new Contact("Benjamin", "Cayton", "cayton10@live.marshall.edu", "3046389603"));
+            Contacts.Add(new Contact("Benjamin", "Cayton", "cayton10@live.marshall.edu", "3046389603"));
+            Contacts.Add(new Contact("Benjamin", "Cayton", "cayton10@live.marshall.edu", "3046389603"));
+            Contacts.Add(new Contact("Benjamin", "Cayton", "cayton10@live.marshall.edu", "3046389603"));
+            Contacts.Add(new Contact("Benjamin", "Cayton", "cayton10@live.marshall.edu", "3046389603"));
+            Contacts.Add(new Contact("Benjamin", "Cayton", "cayton10@live.marshall.edu", "3046389603"));
+            Contacts.Add(new Contact("Benjamin", "Cayton", "cayton10@live.marshall.edu", "3046389603"));
+            Contacts.Add(new Contact("Benjamin", "Cayton", "cayton10@live.marshall.edu", "3046389603"));
+            Contacts.Add(new Contact("Benjamin", "Cayton", "cayton10@live.marshall.edu", "3046389603"));
+            Contacts.Add(new Contact("Benjamin", "Cayton", "cayton10@live.marshall.edu", "3046389603"));
+            Contacts.Add(new Contact("Benjamin", "Cayton", "cayton10@live.marshall.edu", "3046389603"));
+            Contacts.Add(new Contact("Benjamin", "Cayton", "cayton10@live.marshall.edu", "3046389603"));
+            Contacts.Add(new Contact("Benjamin", "Cayton", "cayton10@live.marshall.edu", "3046389603"));
+            Contacts.Add(new Contact("Benjamin", "Cayton", "cayton10@live.marshall.edu", "3046389603"));
+            Contacts.Add(new Contact("Benjamin", "Cayton", "cayton10@live.marshall.edu", "3046389603"));
+
+            var lv = FindViewById<ListView>(Resource.Id.contactListView);
+
+            lv.Adapter = new ArrayAdapter<Contact>(this, Android.Resource.Layout.SimpleListItem1, Contacts);
+
+            var addContactButton = FindViewById<Button>(Resource.Id.addContact);
+
+
+        }
+
+
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            //base.OnActivityResult(requestCode, resultCode, data);
+
+            if(requestCode == 100 && resultCode == Result.Ok)
+            {
+                string name = data.GetStringExtra("FirstName");
+            }
+        }
+
+
+        /*private void Contacts_Push(object sender, EventArgs e)
+        {
+            var intent = new Intent(this, typeof(ContactsActivity));
+            StartActivity(intent);
+        }*/
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
